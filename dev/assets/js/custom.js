73b67568c6dcbe961d1dcae066eddd6a116e57fa
Serhii-Vidulin <freederikserjio@gmail.com>
2023-05-22 17:23:45 +0300

    header

--- dev/assets/js/custom.js
+++ dev/assets/js/custom.js
@@ -2,7 +2,7 @@
 
 $(document).ready(function () {
 	console.log("ready!");
-	$('.single-item').slick();
+	$('.first-screen-slider').slick();
 
 	Fancybox.bind("[data-fancybox]", {
 		// Your custom options
