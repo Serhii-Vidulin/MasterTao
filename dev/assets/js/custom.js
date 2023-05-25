f0b22768e033e09597e3a00791c7728cce4e64d7
Serhii-Vidulin <freederikserjio@gmail.com>
2023-05-26 00:31:13 +0300

    first-screen 1/2

--- dev/assets/js/custom.js
+++ dev/assets/js/custom.js
@@ -2,7 +2,22 @@
 
 $(document).ready(function () {
 	console.log("ready!");
-	$('.first-screen-slider').slick();
+	$('.first-screen-slider').slick({
+		dots: true,
+		speed: 2000,
+		arrows: true,
+		appendArrows: ".container-arrows",
+
+		responsive: [
+			{
+				breakpoint: 991,
+				settings: {
+					arrows: false,
+				}
+			},
+
+		]
+	});
 
 	Fancybox.bind("[data-fancybox]", {
 		// Your custom options
